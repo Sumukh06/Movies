abe16bcdac2960a93baa82a7cf035fd0ecea2008
Sumukh <sumukhn06@gmail.com>
2022-02-10 20:14:19 +0530

    @Id

--- movies-info-service/src/main/java/com/learning/moviesinfo/domain/MovieInfo.java
+++ movies-info-service/src/main/java/com/learning/moviesinfo/domain/MovieInfo.java
@@ -3,6 +3,7 @@ package com.learning.moviesinfo.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Document
 public class MovieInfo {
+    @Id
     private String movieInfoId;
     private String name;
     private Integer year;
