216c53f1a0af35f51994c765ed740c81d7d3e1f3
Sumukh <sumukhn06@gmail.com>
2022-02-10 17:36:10 +0530

    Domain and repository

---
+++ movies-info-service/src/main/java/com/learning/moviesinfo/domain/MovieInfo.java
@@ -0,0 +1,21 @@
+package com.learning.moviesinfo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class MovieInfo {
+    private String movieInfoId;
+    private String name;
+    private Integer year;
+    private List<String> cast;
+    private LocalDate release_date;
+}
