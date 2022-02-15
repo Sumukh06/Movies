aeb91bec07299943ca3f8bff04325f61d72b93ac
Sumukh <sumukhn06@gmail.com>
2022-02-15 14:27:13 +0530

    basic setup

---
+++ movie-review-service/src/test/java/com/learning/moviereviewservice/MovieReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.moviereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
